3ecd97a2e10f44e6fd953c0422ce271e8eaecd70
李貌 <limao@startcalife.com>
2018-02-07 02:30:31 +0800

    更改 eslint 规则提示为警告

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,10 +17,24 @@ module.exports = {
   ],
   'rules': {
     // allow paren-less arrow functions
-    'arrow-parens': 0,
+    'arrow-parens': 1,
     // allow async-await
-    'generator-star-spacing': 0,
+    'generator-star-spacing': 1,
+
+    /*eslint : ["error", "double", { "avoidEscape": true }]*/
+
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+
+
+    // 禁止不必要的分号
+    "no-extra-semi": 1,
+    "quotes": 1,
+    "indent": 1,
+    'keyword-spacing' : 1,
+    'space-before-function-paren' : 1,
+    'space-infix-ops' : 1,
+    'no-multiple-empty-lines' : 1,
+
   }
 }
