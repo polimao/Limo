6e0fc05ac1f6ab41e6ebbc544391c4a6b59e8de2
李貌 <limao@startcalife.com>
2018-02-08 00:56:45 +0800

    增加 vue-codemirror

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -7,7 +7,18 @@ import store from './store'
 import db from './datastore'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+
+// ================ 02-08
 import 'codemirror/lib/codemirror.css'
+// 主题载入
+// import 'codemirror/theme/base16-dark.css'
+
+// 载入 codemirror 组件
+import VueCodemirror from 'vue-codemirror'
+Vue.use(VueCodemirror /* {
+  options: { theme: 'base16-dark', ... },
+  events: ['scroll', ...]
+} */)
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 
