001a2dcb2dd10900a7f977ecea4f9c1c6e67edb3
李貌 <limao@startcalife.com>
2018-02-08 00:57:09 +0800

    codemirror 实例

--- src/renderer/components/CodeMirror.vue
+++ src/renderer/components/CodeMirror.vue
@@ -1,44 +1,58 @@
 <template>
 <main>
-  <h1>Hello World!</h1>
-  <codemirror v-model='code' :options='cmOption'></codemirror>
+  <!-- bidirectional data binding（双向数据绑定） -->
+  <codemirror v-model="code" :options="cmOptions"></codemirror>
 
+  <!-- or to manually control the datasynchronization（或者手动控制数据流，需要像这样手动监听changed事件） -->
+  <codemirror ref="myCm"
+              :value="code" 
+              :options="cmOptions"
+              @ready="onCmReady"
+              @focus="onCmFocus"
+              @input="onCmCodeChange">
+  </codemirror>
 </main>
-
 </template>
 
 <script>
-    //   import CodeMirror from 'codemirror/lib/codemirror'
+
+// import 'codemirror/theme/base16-dark.css'
+
 export default {
   data () {
-        return {
-          code: 'const A = 10',
-          cmOption: {
-            tabSize: 4,
-            styleActiveLine: true,
-            lineNumbers: true,
-            line: true,
-            foldGutter: true,
-            styleSelectedText: true,
-            mode: 'text/javascript',
-            keyMap: 'sublime',
-            matchBrackets: true,
-            showCursorWhenSelecting: true,
-            theme: 'monokai',
-            extraKeys: { 'Ctrl': 'autocomplete' },
-            hintOptions: {
-              completeSingle: false
-            }
-          }
-        }
+    return {
+      code: 'const a = 10',
+      cmOptions: {
+        // codemirror options
+        tabSize: 4,
+        mode: 'text/javascript',
+        theme: 'base16-dark',
+        lineNumbers: true,
+        line: true,
+        // more codemirror options, 更多 codemirror 的高级配置...
+      }
+    }
+  },
+  methods: {
+    onCmReady(cm) {
+      console.log('the editor is readied!', cm)
+    },
+    onCmFocus(cm) {
+      console.log('the editor is focus!', cm)
+    },
+    onCmCodeChange(newCode) {
+      console.log('this is new code', newCode)
+      this.code = newCode
+    }
+  },
+  computed: {
+    codemirror() {
+      return this.$refs.myCm.codemirror
+    }
+  },
+  mounted() {
+    console.log('this is current codemirror object', this.codemirror)
+    // you can use this.codemirror to do something...
   }
 }
 </script>
-
-<style lang="scss">
-  // @import './sass/mixins';
-  // @import './sass/variables';
-  main {
-    position: relative;
-  }
-</style>
