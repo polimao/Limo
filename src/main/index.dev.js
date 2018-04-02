fa6dbfa8958dc1db460e42d872cec4188dbe7c86
yepolite <710898853@qq.com>
2018-04-02 23:12:29 +0800

    lol

--- src/main/index.dev.js
+++ src/main/index.dev.js
@@ -11,7 +11,7 @@
 process.env.NODE_ENV = 'development'
 
 // Install `electron-debug` with `devtron`
-require('electron-debug')({ showDevTools: true })
+require('electron-debug')({ showDevTools: false })
 
 // Install `vue-devtools`
 require('electron').app.on('ready', () => {
@@ -30,9 +30,6 @@ require('./index')
 // const { remote } = require('electron')
 // const { app } = remote
 
-
-
-
 //   2018 03-06 注释
 // var menubar = require('menubar')
 
@@ -48,3 +45,89 @@ require('./index')
 //   // Menu.setApplicationMenu(menu)
 // })
 //   2018 03-06 注释
+
+import { Menu } from 'electron'
+// ---------------------------------------------------------------------------------------------------
+// 注册常用快捷键，解决 mac 上不能用复制粘贴问题
+// const Menu = require("menu");
+// Create the Application's main menu
+let template = [
+  {
+    label: 'Application',
+    submenu: [
+      {
+        label: 'About Hosts High',
+        click: function() {
+          // new BrowserWindow({
+          //   parent: BrowserWindow.getFocusedWindow(),
+          //   width: 285,
+          //   height: 230,
+          //   center: true,
+          //   resizable: false,
+          //   minimizable: false,
+          //   maximizable: false,
+          //   show: true,
+          //   title: ''
+          // }).loadURL('file://' + resolve(__dirname, 'about.html'))
+        }
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Quit Hosts High',
+        accelerator: 'CmdOrCtrl+Q',
+        click: function() {
+          app.quit()
+        }
+      }
+    ]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        label: 'Undo',
+        accelerator: 'CmdOrCtrl+Z',
+        selector: 'undo:',
+        role: 'undo'
+      },
+      {
+        label: 'Redo',
+        accelerator: 'Shift+CmdOrCtrl+Z',
+        selector: 'redo:',
+        role: 'redo'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Cut',
+        accelerator: 'CmdOrCtrl+X',
+        selector: 'cut:',
+        role: 'cut'
+      },
+      {
+        label: 'Copy',
+        accelerator: 'CmdOrCtrl+C',
+        selector: 'copy:',
+        role: 'copy'
+      },
+      {
+        label: 'Paste',
+        accelerator: 'CmdOrCtrl+V',
+        selector: 'paste:',
+        role: 'paste'
+      },
+      {
+        label: 'Select All',
+        accelerator: 'CmdOrCtrl+A',
+        selector: 'selectAll:',
+        role: 'selectall'
+      }
+    ]
+  }
+]
+// 注册菜单
+Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+// ---------------------------------------------------------------------------------------------------
