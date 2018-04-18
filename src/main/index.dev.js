ce6a96c22ed064ebac03ae55507dab698d428987
yepolite <710898853@qq.com>
2018-04-19 01:11:36 +0800

    menubar

--- src/main/index.dev.js
+++ src/main/index.dev.js
@@ -6,6 +6,7 @@
  */
 
 /* eslint-disable */
+var path = require('path')
 
 // Set environment for development
 process.env.NODE_ENV = 'development'
@@ -26,52 +27,59 @@ require('electron').app.on('ready', () => {
     })
 })
 
-// Require `main` process to boot app
-require('./index')
-
 // const { remote } = require('electron')
 // const { app } = remote
 
 //   2018 03-06 注释
-var menubar = require('menubar')
-var opts = {
-  dir: __dirname + '/../menubar',
-  width: 200,
-  height: 150
-  // alwaysOnTop: true
-  // showOnRightClick: true
-}
-var mb = menubar(opts)
+// var menubar = require('menubar')
+
+// var mb = menubar({
+//   dir: path.join(__dirname, '/../menubar'),
+//   width: 200,
+//   height: 150
+//   // alwaysOnTop: true
+//   // showOnRightClick: true
+// })
 // var mb = menubar()
 
 // console.log('dev ----- ')
-mb.on('ready', function ready() {
-  // console.log('--limao---')
-  // console.log(app.getAppPath())
-  // Build default menu for text editing and devtools. (gone since electron 0.25.2)
-  // var menu = Menu.buildFromTemplate(template)
-  // Menu.setApplicationMenu(menu)
-})
-
-
-
+// mb.on('after-create-window', function ready() {
+//   // console.log('--limao---')
+//   // console.log(app.getAppPath())
+//   // Build default menu for text editing and devtools. (gone since electron 0.25.2)
+//   // var menu = Menu.buildFromTemplate(template)
+//   // Menu.setApplicationMenu(menu)
+// })
 // ========== 04-17 农历 menubar
-var menubar2 = require('menubar')
-var opts = {
-  dir: __dirname + '/../menubar/lunar',
-  width: 540,
-  height: 425
-  // alwaysOnTop: true
-  // showOnRightClick: true
-}
-var mb2 = menubar2(opts)
+
+// var mb2 = menubar({
+//   dir: path.join(__dirname, '/../menubar/lunar'),
+//   width: 540,
+//   height: 425
+//   // alwaysOnTop: true
+//   // showOnRightClick: true
+// })
 // var mb = menubar()
 
 // console.log('dev ----- ')
-mb2.on('ready', function ready() {
-  // console.log('--limao---')
-  // console.log(app.getAppPath())
-  // Build default menu for text editing and devtools. (gone since electron 0.25.2)
-  // var menu = Menu.buildFromTemplate(template)
-  // Menu.setApplicationMenu(menu)
+// mb2.on('after-create-window', function ready() {
+//   // console.log('--limao---')
+//   // console.log(app.getAppPath())
+//   // Build default menu for text editing and devtools. (gone since electron 0.25.2)
+//   // var menu = Menu.buildFromTemplate(template)
+//   // Menu.setApplicationMenu(menu)
+// })
+// console.log(iconPath)
+var menubar = require('menubar')
+menubar({
+  dir: path.join(__dirname, '/../menubar/lunar'),
+  width: 540,
+  height: 425,
+  // icon: iconPath
+  // icon: path.join(__dirname, '//../menubar/lunar/', 'panda.png').replace(/\\/g, '\\\\'),
+  // alwaysOnTop: true
+  // showOnRightClick: true
 })
+
+// Require `main` process to boot app
+require('./index')
